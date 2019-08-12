9b9e1a7e2b4e358ad3dfa8e59c81b06f66582fc5
AnthonyClausing <AnthonyClausing@gmail.com>
2019-08-12 19:58:04 -0400

    fix for db connection?

--- db/db.js
+++ db/db.js
@@ -1,12 +1,25 @@
 const Sequelize = require('sequelize')
 require('sequelize-hierarchy')(Sequelize)
 const {DB_NAME, DB_USER, DB_PASSWORD} = process.env
-const db = new Sequelize(
-  process.env.DATABASE_URL || DB_NAME, DB_USER, DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false
-  }
-)
+var db;
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  db = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+  })
+} else {
+  db =  new Sequelize(
+    process.env.DATABASE_URL || DB_NAME, DB_USER, DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      logging: false
+    }
+  )
+}
+
 
 module.exports = db
